a2f270a4b1dc6e999b6bfa52b91ac9b849011a06
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-07-25 14:42:35 +0900

    backend init

---
+++ src/main/java/com/runningpartybe/RunningpartybeApplication.java
@@ -0,0 +1,13 @@
+package com.runningpartybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunningpartybeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RunningpartybeApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/runningpartybe/RunningpartybeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runningpartybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunningpartybeApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
