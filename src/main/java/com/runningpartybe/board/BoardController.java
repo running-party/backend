84f2371a2b017113660af33f934b0fa1221020fb
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-08-09 13:58:23 +0900

    feat: board read data

---
+++ src/main/java/com/runningpartybe/board/BoardController.java
@@ -0,0 +1,25 @@
+package com.runningpartybe.board;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:8081")
+public class BoardController {
+
+  @GetMapping("/boards")
+  public ResponseEntity<List<Board>> getBoards() {
+    List<Board> boards = new ArrayList<>();
+    boards.add(new Board(1, "First Board", "Running Party!"));
+    boards.add(new Board(2, "Second Board", "keep going!"));
+    return ResponseEntity.ok(boards);
+  }
+}
+
