6555946ba4a979e4577834b3e2381f5bf7c51004
k-jaehyun <wogus146@gmail.com>
2024-08-12 01:07:45 +0900

    모든 출처 요청에 대해 허용

--- src/main/java/com/runningpartybe/board/BoardController.java
+++ src/main/java/com/runningpartybe/board/BoardController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:8081")
+@CrossOrigin(origins = "*")
 public class BoardController {
 
   @GetMapping("/boards")
