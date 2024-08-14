88e55e47fbe7f158c9aa15862b1caf93843e7f66
k-jaehyun <wogus146@gmail.com>
2024-08-15 00:45:17 +0900

    fix: RunningRecord Entity, user와 연관관계 설정 - 지연로딩

--- src/main/java/com/runningpartybe/domain/runningrecord/entiry/RunningRecord.java
+++ src/main/java/com/runningpartybe/domain/runningrecord/entiry/RunningRecord.java
@@ -4,6 +4,7 @@ import com.runningpartybe.domain.user.entity.User;
 import com.runningpartybe.global.entity.Timestamped;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -25,7 +26,7 @@ public class RunningRecord extends Timestamped {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @ManyToOne
+  @ManyToOne(fetch = FetchType.LAZY) // 성능 최적화를 위한 지연로딩
   @JoinColumn(name = "user_id")
   private User user;
 
