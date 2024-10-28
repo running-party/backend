e891a246c74c5ea47c75c3a3078536cf719ccd8c
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-10-28 20:32:05 +0900

    fix: UserController, UserRepository

--- src/main/java/com/runningpartybe/domain/user/controller/UserController.java
+++ src/main/java/com/runningpartybe/domain/user/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@CrossOrigin(origins = "http://localhost:53419")
+@CrossOrigin(origins = "http://localhost:8000")
 @RequestMapping("/v1/user")
 public class UserController {
 
--- src/main/java/com/runningpartybe/domain/user/repository/UserRepository.java
+++ src/main/java/com/runningpartybe/domain/user/repository/UserRepository.java
@@ -2,7 +2,12 @@ package com.runningpartybe.domain.user.repository;
 
 import com.runningpartybe.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+  // 추가적인 메서드 작성 (Optional: 예시로 사용자 ID로 조회하는 메서드)
+  Optional<User> findByLoginId(String loginId);
 
+  // 추가적으로 이메일로 찾는 메서드
+  Optional<User> findByEmail(String email);
 }
