d315eba865dd7ef8713cdb06aa4a7b7969f28e6b
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-10-28 20:29:08 +0900

    fix: AuthController, AuthEntity

--- src/main/java/com/runningpartybe/domain/auth/controller/AuthController.java
+++ src/main/java/com/runningpartybe/domain/auth/controller/AuthController.java
@@ -1,8 +1,8 @@
-package com.runningpartybe.controller;
+package com.runningpartybe.domain.auth.controller;
 
+import com.runningpartybe.domain.auth.service.AuthService;
 import com.runningpartybe.dto.auth.request.AuthRequestDto;
 import com.runningpartybe.dto.auth.response.AuthResponseDto;
-import com.runningpartybe.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,6 +15,7 @@ public class AuthController {
   private final AuthService authService;
 
   // 일반 로그인 처리
+  @CrossOrigin(origins = "http://localhost:8000")
   @PostMapping("/login")
   public ResponseEntity<AuthResponseDto> login(@RequestBody AuthRequestDto authRequestDto) {
     // 로그인 ID와 비밀번호가 있는 경우 일반 로그인으로 처리
--- src/main/java/com/runningpartybe/domain/auth/entity/Auth.java
+++ src/main/java/com/runningpartybe/domain/auth/entity/Auth.java
@@ -15,6 +15,7 @@ import java.time.LocalDateTime;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@Table(name = "auth")
 public class Auth {
 
   @Id
