b7b2c5b3ee763daf566e7acc7f9bd541f1fcf508
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-11-11 13:40:34 +0900

    add: JwtProvider

---
+++ src/main/java/com/runningpartybe/global/config/JwtProvider.java
@@ -0,0 +1,46 @@
+package com.runningpartybe.global.config;
+
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtProvider {
+
+    @Value("${secretKey}")
+    private String secretKey;
+
+    public String create(String email) {
+        System.out.println("secretKey");
+
+        Date expiredDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        String jwt = Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setSubject(email).setIssuedAt(new Date()).setExpiration(expiredDate).compact();
+
+        return jwt;
+    }
+
+    public String validate(String jwt) {
+        Claims claims = null;
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        try {
+            claims = Jwts.parserBuilder()
+                    .setSigningKey(key)
+                    .build()
+                    .parseClaimsJws(jwt)
+                    .getBody();
+        } catch (Exception exception) {
+            exception.printStackTrace();
+            return null;
+        }
+        return claims.getSubject();
+    }
+}
